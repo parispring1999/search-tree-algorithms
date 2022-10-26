bdc00eb4ad27c8c975395979dfa0c49f0f868e59
parispring <3059655099@qq.com>
2022-10-26 20:29:34 +0800

    修改

--- Problems/Fill_array_problem/FillArrayTest.py
+++
@@ -1,9 +0,0 @@
-
-import sys
-from tree          import *
-from queue_search  import *
-from FillArray  import *
-
-problem = make_fill_problem(10)
-
-search(problem, 'depth_first', 100000, ['loop_check'])
--- Problems/Fill_list_problem/FillTester.py
+++
@@ -1,9 +0,0 @@
-
-import sys
-from tree          import *
-from queue_search  import *
-from FillProblem  import *
-
-problem = make_fill_problem(4)
-
-search(problem, 'breadth_first', 2000, [])
